2b3ff12ee7bf5f1332d0beafe3c75f31a154a4e8
Victor de Carvalho Silva <vtrcarvalho.13@gmail.com>
2020-09-23 22:09:42 -0300

    Correção

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ app.use(express.static(path.join(__dirname, 'client/build')));
  * Rota raiz
  */
 app.get('/api/', (_, response) => {
-  response.send({
-    message:
-      'Bem-vindo à API de lançamentos. Acesse /transaction e siga as orientações',
-  });
+    response.send({
+        message:
+            'Bem-vindo à API de lançamentos. Acesse /transaction e siga as orientações',
+    });
 });
 
 /**
@@ -38,35 +38,36 @@ app.use('/api/transaction', routes);
 /**
  * Conexão ao Banco de Dados
  */
-const { DB_CONNECTION } = process.env;
 
+const { DB_CONNECTION } = process.env;
+console.log(DB_CONNECTION);
 console.log('Iniciando conexão ao MongoDB...');
 mongoose.connect(
-  DB_CONNECTION,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) {
-      connectedToMongoDB = false;
-      console.error(`Erro na conexão ao MongoDB - ${err}`);
+    DB_CONNECTION,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) => {
+        if (err) {
+            connectedToMongoDB = false;
+            console.error(`Erro na conexão ao MongoDB - ${err}`);
+        }
     }
-  }
 );
 
 const { connection } = mongoose;
 
 connection.once('open', () => {
-  connectedToMongoDB = true;
-  console.log('Conectado ao MongoDB');
+    connectedToMongoDB = true;
+    console.log('Conectado ao MongoDB');
 
-  /**
-   * Definição de porta e
-   * inicialização do app
-   */
-  const APP_PORT = process.env.PORT || 3001;
-  app.listen(APP_PORT, () => {
-    console.log(`Servidor iniciado na porta ${APP_PORT}`);
-  });
+    /**
+     * Definição de porta e
+     * inicialização do app
+     */
+    const APP_PORT = process.env.PORT || 3001;
+    app.listen(APP_PORT, () => {
+        console.log(`Servidor iniciado na porta ${APP_PORT}`);
+    });
 });
